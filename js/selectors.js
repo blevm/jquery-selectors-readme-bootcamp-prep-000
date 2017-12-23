ceec027179925b15d4da7801e515b282a2a43e41
Michelle B <mjblevins7@gmail.com>
2017-12-23 23:41:57 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,5 +1,25 @@
 // declare your functions here...
 function paragraphSelector() {
-  let p = $('p')
+  let p = $('p');
   return p;
 };
+
+function lastImageSelector() {
+  let lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  let babyID = $('#baby-ninja');
+  return babyID;
+}
+
+function divSelector() {
+  let divs = $('div.pics');
+  return divs;
+}
+
+function firstListItem() {
+  let firstList = $('ul#pic-list:first-child');
+  return firstList;
+}
